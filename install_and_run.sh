efe1fd35ecbd3b839ef1501552f40aca21c2f279
Linus Hakansson <linus.hakansson@oracle.com>
2018-05-17 12:19:20 +0200

    Python changes

--- install_and_run.sh
+++ install_and_run.sh
@@ -2,7 +2,6 @@
 
 # download dependencies
 pip install -r requirements.txt
-pip install mysql-python 
 
 # run app
 python app.py
--- python-service/start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export PYTHONPATH=modules
-python app.py 
