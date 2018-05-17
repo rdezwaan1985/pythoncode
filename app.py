0a5370e781628a77329b7966dd6c6e775c525e49
Linus <linushakansson89@gmail.com>
2018-05-17 18:13:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def home():
     return render_template('home.html')
 
 def rest_request_example():
-    print (requests.get("http://129.157.179.180:3000/shield/33/45/blue/oracledevstar").text)
+    print (requests.get("http://ip.jsontest.com/").text)
 
 def read_db_SQL_example():
     conn = db.get_engine().connect()
