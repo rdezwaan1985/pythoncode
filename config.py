3c99a7be8afbedc75184d2dbf70ddf8c4557b241
rdezwaan1985 <40593624+rdezwaan1985@users.noreply.github.com>
2018-06-26 09:56:01 +0000

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 class Configuration(object):
     DEBUG = True
     PORT = int(os.getenv('PORT','22222'))
-    HOST = '0.0.0.0'
+    HOST = '140.86.15.104'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql://' \
-            + os.getenv('MYSQLCS_USER_NAME','root') + ':' \
-            + os.getenv('MYSQLCS_USER_PASSWORD','password') + '@' \
+            + os.getenv('MYSQLCS_USER_NAME','Captain') + ':' \
+            + os.getenv('MYSQLCS_USER_PASSWORD','welcome1') + '@' \
             + os.getenv('MYSQLCS_CONNECT_STRING', \
-            '127.0.0.1:3306/sample_db')
+            '140.86.15.104/deathstar')
